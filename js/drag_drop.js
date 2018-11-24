aec339d3cab87d4988d9f826df2413d55f383b25
Ash-247 <abirdi@outlook.com>
2018-11-23 22:03:57 -0800

    Rough Drag and drop (now snaps to grid)
    Very inefficient, problems with scroll bar.
    Still shows functionality.

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -1,27 +1,33 @@
-function myFunction() {
-    var x = $(".num").length - 1;
-    document.getElementById("demo").innerHTML = x;
-}
+// function myFunction() {
+//     var x = $(".num").length - 1;
+//     document.getElementById("demo").innerHTML = x;
+// }
+
 
 $(function() {
     $("li").hide(0).slideDown(1100);
     $("ul").sortable();
     $("ul").disableSelection();
 
+ //    $("li").draggable({containment: 'document', revert: true, snap: '.semester', scroll: false, helper: "clone",
+ //    	start: function() {
+ //    		var content = $(this).text();
+ //   		}
+	// });
 
-    $("li").draggable({containment: 'document', revert: true, snap: '.semester', scroll: false, helper: "clone",
-    	start: function() {
-    		var content = $(this).text();
-   		}
-	});
+ //    $(".semester").droppable({hoverClass: 'bgrd', accept: '.item', //Jquery 'hover' instead of CSS
+ //        drop: function() {
+ //        	$(this).addClass("num");
+ //        	$("td").append(content);
 
-    $(".semester").droppable({hoverClass: 'bgrd', accept: '.item', //Jquery 'hover' instead of CSS
-    drop: function() {
-    	$(this).addClass("num");
-    	$("td").append(content);
+ //        	$(".num").length;
+ //        }
+ //    });
 
-    	$(".num").length;
-    }
-
-    });
 });
+
+$( function() {
+    $( "#sortable1, #sortable2" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  } );
