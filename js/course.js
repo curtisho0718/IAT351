a998806a6447990e778b3ed56807e5dcc52a6f1b
Curtis Ho <curtisho0718@hotmail.com>
2018-11-24 12:06:18 -0800

    Update course.js

--- js/course.js
+++ js/course.js
@@ -1,10 +1,16 @@
 var request = new XMLHttpRequest();
-request.open('GET','http://www.sfu.ca/bin/wcm/course-outlines');
+request.open('GET', 'http://www.sfu.ca/bin/wcm/course-outlines');
 request.onload = function () {
 	// begin accessing JSON data here
 	var data = JSON.parse(this.response);
-	for (var i = 0; i < data.length; i++) {
-		console.log(data[i].text);
+
+	if (this.readyState == 4 && this.status == 200) {
+		var dropdown= document.getElementById('dropdown');
+		for (var i = 0; i < data.length; i++) {
+			console.log(data[i].text);
+			dropdown.innerHTML=dropdown.innerHTML+'<option value="'+data[i].text+'</option>';
+		}
 	}
 }
+
 request.send();
