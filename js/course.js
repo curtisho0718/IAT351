acc656aef916e515651308319f83c40dadf751ac
Curtis Ho <curtisho0718@hotmail.com>
2018-11-24 12:58:00 -0800

    add archive folder

--- js/course.js
+++ js/course.js
@@ -1,16 +1,34 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'http://www.sfu.ca/bin/wcm/course-outlines');
-request.onload = function () {
-	// begin accessing JSON data here
-	var data = JSON.parse(this.response);
+window.onload=populateSelect();
 
-	if (this.readyState == 4 && this.status == 200) {
-		var dropdown= document.getElementById('dropdown');
-		for (var i = 0; i < data.length; i++) {
-			console.log(data[i].text);
-			dropdown.innerHTML=dropdown.innerHTML+'<option value="'+data[i].text+'</option>';
+function populateSelect(){
+	var request=new XMLHttpRequest();
+	request.open('GET','http://www.sfu.ca/bin/wcm/course-outlines');
+
+	request.onreadystatechange=function(){
+		if(request.readyState===XMLHttpRequest.DONE && request.status===200){
+			var data =JSON.parse(request.responseText);
+			var ele=document.getElementById('sel');
+			var year= 
+			for(var i=0; i<year.length, i++){
+
+			}
 		}
 	}
+	
 }
+// var request = new XMLHttpRequest();
+// request.open('GET', 'http://www.sfu.ca/bin/wcm/course-outlines');
+// request.onload = function () {
+// 	// begin accessing JSON data here
+// 	var data = JSON.parse(this.response);
 
-request.send();
+
+// 		var dropdown= document.getElementById('dropdown');
+// 		for (var i = 0; i < data.length; i++) {
+// 			console.log(data[i].text);
+// 			// dropdown.innerHTML=dropdown.innerHTML+'<option value="'+data[i].text+'</option>';
+// 		}
+// 	}
+
+
+// request.send();
