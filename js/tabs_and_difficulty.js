a9f1285b53fbbe9caa517a54c45c76418e06156c
Nicholas Ko <ntko@sfu.ca>
2018-11-24 21:21:56 -0800

    Revert "Combined JS files"
    This reverts commit 9935a59656acc03f7af118ee4b2510bdd846e717.

---
+++ js/tabs_and_difficulty.js
@@ -0,0 +1,97 @@
+'use strict';
+
+console.log("tabs_and_difficulty.js JavaScript is online.");
+
+var table_shown = 1;
+
+window.onload = function () {
+    $("#schedule1").show();
+    $("#schedule2").hide();
+    table_shown = 1;
+}
+
+$("#tab_button1").click(function () {
+    $("#schedule1").show();
+    $("#schedule2").hide();
+    table_shown = 1;
+});
+
+$("#tab_button2").click(function () {
+    $("#schedule1").hide();
+    $("#schedule2").show();
+    table_shown = 2;
+});
+
+function addEasyCourse() {
+    if (table_shown == 1) {
+        document.getElementById('year1_fall1_tab1_difficulty1').value += 15;
+    } else if (table_shown == 2) {
+        document.getElementById('year1_fall1_tab1_difficulty2').value += 15;
+    }
+
+    // var individual_bars = document.getElementById('year1_fall1_tab1_difficulty')
+    // for (var i = 0; i < 10  ; i++) {
+    //     if (table_shown == i) {
+    //         for (var j = 0; j < individual_bars.length; j++) {
+    //         individual_bars[j].value += 15;
+    //         }
+    //     }
+    // }
+}
+
+function removeEasyCourse() {
+    document.getElementById('year1_fall1_tab1_difficulty').value -= 15;
+}
+
+function addNormalCourse() {
+    document.getElementById('year1_fall1_tab1_difficulty').value += 25;
+}
+
+function removeNormalCourse() {
+    document.getElementById('year1_fall1_tab1_difficulty').value -= 25;
+}
+
+function addHardCourse() {
+    document.getElementById('year1_fall1_tab1_difficulty').value += 40;
+}
+
+function removeHardCourse() {
+    document.getElementById('year1_fall1_tab1_difficulty').value -= 40;
+}
+
+function add233() {
+    document.getElementById('year1_fall1_tab1_difficulty').value += 50;
+}
+
+function remove233() {
+    document.getElementById('year1_fall1_tab1_difficulty').value -= 50;
+}
+
+function add438() {
+    document.getElementById('year1_fall1_tab1_difficulty').value += 75;
+}
+
+function remove438() {
+    document.getElementById('year1_fall1_tab1_difficulty').value -= 75;
+}
+
+function fire() {
+    var progressbar = document.getElementsByTagName('progress');
+    for (var i = 0; i < progressbar.length; i++) {
+        if (progressbar[i].value < 61) {
+            $(progressbar[i]).removeClass("difficulty_bar_red");
+            $(progressbar[i]).removeClass("difficulty_bar_yellow");
+            $(progressbar[i]).addClass("difficulty_bar_green");
+            console.log("Ba.");
+        } else if (progressbar[i].value > 75) {
+            $(progressbar[i]).removeClass("difficulty_bar_yellow");
+            $(progressbar[i]).removeClass("difficulty_bar_green");
+            $(progressbar[i]).addClass("difficulty_bar_red");
+        } else {
+            $(progressbar[i]).removeClass("difficulty_bar_green");
+            $(progressbar[i]).removeClass("difficulty_bar_red");
+            $(progressbar[i]).addClass("difficulty_bar_yellow");
+            console.log("Ta.");
+        }
+    }
+}
